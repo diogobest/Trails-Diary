080d5677be4cd94c1042e13f9c9c4080c5549048
Diogo <diogo.oliveira44@gmail.com>
2019-11-17 21:12:30 -0300

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-#  scope "(:locale)", locale: /en|pt/ do
     root to: 'home#index'
-#  end
-  get '/:locale' => 'home#index'
+    resources :profiles, only: %i[new show edit create]
 end
