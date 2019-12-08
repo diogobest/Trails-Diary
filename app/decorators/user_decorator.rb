9284ed08f91b17839f4fd88fc6e7d1241d1a9aba
Diogo <diogo.oliveira44@gmail.com>
2019-12-08 19:57:50 -0300

    cards for posts page

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-class UserDecorator < Draper::Decorator
-  delegate_all
+class UserDecorator < SimpleDelegator
 
   def new_session
     h.link_to 'Login', new_user_session_path
@@ -9,5 +8,9 @@ class UserDecorator < Draper::Decorator
 
   def delete_session
     h.link_to 'logout', destroy_user_session_path, method: :delete
+  end
+  
+  def h
+    ApplicationController.helpers
   end
 end
