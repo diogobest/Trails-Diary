c1baa212e4a69236178d686fe032790d174c30c7
Diogo <diogo.oliveira44@gmail.com>
2019-12-24 15:06:27 -0300

    removed logs and added bootstrap for profile page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-   before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: %i[index]
 
   def index
-    return redirect_to new_profile_path if current_user.profile.nil?
-
+    # return redirect_to new_profile_path if current_user.profile.nil?
     @posts = Post.all
   end
 end
